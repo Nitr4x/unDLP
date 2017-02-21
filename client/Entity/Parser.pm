d0913cf881a8a8ac1b2bcb625796469284de8330
lejeun_j <jean.lejeune@bssi.fr>
2017-02-21 13:29:08 +0100

    Add parser

---
+++ client/Entity/Parser.pm
@@ -0,0 +1,62 @@
+package Parser;
+
+use Getopt::Long;
+use Moose;
+
+has delay => (
+    is      =>  'rw',
+    isa     =>  'Int',
+    default =>  0
+);
+
+has encryption => (
+    is      =>  'rw',
+    isa     =>  'Int',
+    default =>  0
+);
+
+has key => (
+    is      =>  'rw',
+    isa     =>  'Str',
+    default =>  ''
+);
+
+has file => (
+    is  =>  'rw',
+    isa =>  'Str'
+);
+
+has help => (
+    is      =>  'rw',
+    isa     =>  'Int',
+    default =>  0
+);
+
+sub parse {
+    my $self = shift;
+    my @args = @_;
+
+    GetOptions(
+        'delay=i' => \$self->{delay},
+        'e' => \$self->{encryption},
+        'key=s' => \$self->{key},
+        'help|h' => \$self->{help}
+    );
+
+    $self->{file} = $args[0];
+
+    if (scalar @args < 1 || $self->help || !$self->file) {
+        usage()
+    }
+}
+
+sub usage {
+    print "\nusage: unDLP.pl FILE [-e] [-delay DELAY] [-key KEY] [--help]\n\n";
+    print "\t -e: Cipher the provided file.\n";
+    print "\t -delay: Set the transfer speed.\n";
+    print "\t -key: Set the public key used for the encryption. If not specified, the key will be retrieved from the server.\n";
+
+    exit;
+}
+
+1;
