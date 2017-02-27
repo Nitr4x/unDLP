96864df2ca8e30416a75a287a9f6004b4775ab7d
lejeun_j <jean.lejeune@bssi.fr>
2017-02-27 15:30:37 +0100

    Adding class factory

--- client/Entity/Parser.pm
+++ client/Entity/Parser.pm
@@ -29,11 +29,17 @@ has help => (
     default =>  0
 );
 
+has method => (
+    is  =>  'rw',
+    isa =>  'Str'
+);
+
 has size => (
     is      =>  'rw',
     isa     =>  'Int',
     default =>  1024
 );
+
 
 sub parse {
     my $self = shift;
@@ -42,20 +48,22 @@ sub parse {
     GetOptions(
         'f=s' => \$self->{file},
         'd=s' => \$self->{dest},
+        'm=s' => \$self->{method},
         'delay=i' => \$self->{delay},
         'size=i' => \$self->{size},
         'help|h' => \$self->{help}
     );
 
-    if (scalar @args < 1 || $self->help || !$self->file || !$self->dest) {
+    if (scalar @args < 1 || $self->help || !$self->file || !$self->dest || !$self->method) {
         usage()
     }
 }
 
 sub usage {
-    print "\nusage: unDLP.pl -f FILE -d DESTINATION [--delay DELAY] [--help|h]\n\n";
+    print "\nusage: unDLP.pl -f FILE -d DESTINATION -m [HTTPS] [--size SIZE] [--delay DELAY] [--help|h]\n\n";
     print "\t -f: File to transfer.\n";
     print "\t -d: Destination.\n";
+    print "\t -m: Exfiltration method.\n";
     print "\t --size: Set the transfer size.\n";
     print "\t --delay: Set the transfer speed (second).\n";
     print "\t --help|h: Display the helper.\n";
