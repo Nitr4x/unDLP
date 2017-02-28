203840d540abb69fb4233310d433c0b64b0b5fcb
lejeun_j <jean.lejeune@bssi.fr>
2017-02-28 15:32:51 +0100

    Remove useless use statement and comments

--- client/unDLP.pl
+++ client/unDLP.pl
@@ -2,8 +2,6 @@
 
 use Parallel::ForkManager;
 use strict;
-# use threads;
-# use Thread::Queue;
 use warnings;
 
 require Entity::ModuleFactory;
@@ -28,35 +26,8 @@ my $parser = Parser->new();
 
 $parser->parse(@ARGV);
 
-my $pm = new Parallel::ForkManager(30);
 my $engine =  ModuleFactory->new()->create($parser);
 
 foreach my $file ($parser->getFiles) {
     $engine->exfiltrate($file);
 }
-
-# my $q = Thread::Queue->new();
-#
-# $q->enqueue($_) for $parser->getFiles;
-#
-# my @thr = map {
-#     threads->create(sub {
-#         while (defined(my $file = $q->dequeue_nb())) {
-#             my $engine = ModuleFactory->new()->create($parser);
-#
-#             $engine->exfiltrate($file)
-#         }
-#     });
-# }1..scalar $parser->getFiles;
-#
-# $_->join() for @thr;
-
-# foreach my $file ($parser->getFiles) {
-#     # my $thr = threads->create(sub {
-#     #     $engine->exfiltrate($file)
-#     # });
-#     #
-#     # $thr->join();
-#     my $thr = async { $engine->exfiltrate($file) };
-#     $thr->join();
-# }
